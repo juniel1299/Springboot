be4590a09475de8c5d0bd96e3df262c57c79770f
juniel1299 <youngs5440@gmail.com>
2024-05-28 13:38:05 +0900

    33

---
+++ boot-hellow/src/main/java/com/test/boot_hellow/BootHellowApplication.java
@@ -0,0 +1,13 @@
+package com.test.boot_hellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHellowApplication.class, args);
+	}
+
+}
